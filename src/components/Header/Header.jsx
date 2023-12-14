53874c1f010e648b602bce7d82a8b4fb17a3440d
Shishir Halder <s.halder0108@gmail.com>
2023-12-14 10:51:18 +0600

    test: add a rule

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -33,13 +33,13 @@ const Header = () => {
         <div className="bg-[#2C3E50] text-[#ECF0F1] pt-8 pb-6 px-4 rounded-xl">
           <div className="flex justify-end mx-3 my-3">
             <div className="avatar">
-              <div className="w-56 rounded-full ring ring-[#ECF0F1] ring-offset-[#2C3E50] ring-offset-8">
+              <div className="w-44 rounded-full ring ring-[#ECF0F1] ring-offset-[#2C3E50] ring-offset-8">
                 <Image src={profilePicM} height={300} width={300} alt="Shishir"></Image>
               </div>
             </div>
           </div>
           <div>
-            <p className="text-4xl font-light">I am</p>
+            <p className="text-4xl font-light">I'm</p>
             <h2 className="text-4xl mt-6">Shishir Halder</h2>
           </div>
         </div>
