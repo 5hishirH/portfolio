090dd730331f9c44681a0448ced718c9a2b84877
Shishir Halder <s.halder0108@gmail.com>
2023-12-18 08:57:01 +0600

    feat: add styles to datebase component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,20 +1,20 @@
 import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
-import { SiDaisyui, SiTailwindcss } from "react-icons/si";
+import { SiDaisyui, SiMongodb, SiTailwindcss } from "react-icons/si";
 
 const Skills = () => {
   return (
     <div className="bg-[#6b574e] pt-4 lg:pt-24">
       <div className="lg:w-3/5 lg:mx-auto mx-8 text-white">
-        <h2 className="text-2xl lg:text-4xl font-extrabold">
+        <h2 className="text-2xl lg:text-3xl font-bold">
           <span className="hidden lg:inline-block">
-            My proficiency encompasses the following skills
+            MY PROFICIENCY ENCOMPASSES THE FOLLOWING SKILLS
           </span>
           <span className="lg:hidden">My skills</span>
         </h2>
         <div>
           <div>
-            <h2 className="text-lg lg:text-2xl font-medium mt-4 lg:mt-8 mb-4">
-              Frontend development
+            <h2 className="text-lg lg:text-xl font-medium mt-4 lg:mt-8 mb-4">
+              Frontend Development
             </h2>
             <ul className="grid grid-cols-2 lg:grid-cols-3 gap-6 text-black">
               <li className="bg-white rounded-md px-3 py-2 lg:px-10 lg:py-4 flex gap-4 items-center lg:text-lg">
@@ -41,15 +41,11 @@ const Skills = () => {
           </div>
           <div>
             <h2 className="text-xl font-medium mt-8 mb-4">Database</h2>
-            <ul className="grid grid-cols-3 gap-6">
-              {["MongoDB"].map((e, i) => (
-                <li
-                  key={i}
-                  className="border-2 border-[#2C3E50] rounded-md px-10 py-4 "
-                >
-                  {e}
-                </li>
-              ))}
+            <ul className="grid grid-cols-3 gap-6 text-black">
+              <li className="bg-white rounded-md px-3 py-2 lg:px-10 lg:py-4 flex gap-4 items-center lg:text-lg">
+                <SiMongodb className="text-2xl lg:text-3xl text-green-600" />
+                MongoDB
+              </li>
             </ul>
           </div>
           <div>
