f47126c7d0010480f61cad9504137c9065cb2a0b
Shishir Halder <s.halder0108@gmail.com>
2023-12-19 09:16:42 +0600

    test: download functionalities

--- src/components/DownloadBtn/Dbtn.jsx
+++ src/components/DownloadBtn/Dbtn.jsx
@@ -1,7 +1,12 @@
 const Dbtn = () => {
   return (
-    <a href="@/assets/resume.txt" download="Shishir.txt">
-      <button className="btn btn-outline btn-block lg:btn-wide btn-secondary lg:btn-primary">Download Resume</button>
+    <a
+      href="https://portfolio-5hishirh.vercel.app/resume.txt"
+      download="Shishir.txt"
+    >
+      <button className="btn btn-outline btn-block lg:btn-wide btn-secondary lg:btn-primary">
+        Download Resume
+      </button>
     </a>
   );
 };
