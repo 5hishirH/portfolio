2177df2d8d84e1e5a68f587c9143690107ceb6ae
Shishir Halder <s.halder0108@gmail.com>
2023-12-22 08:37:46 +0600

    feat: add project1 image
    TrendTrove homepage image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Contact from "@/components/Contact/Contact";
 import Education from "@/components/Education/Education";
 import Header from "@/components/Header/Header";
+import Projects from "@/components/Projects/Projects";
 import Skills from "@/components/Skills/Skills";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Projects />
       <Skills />
       <Education />
       <Contact />
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const Projects = () => {
+  const projects = [
+    {
+      name: "TrendTrove",
+      info: "A fashion and apparel selling website",
+      githubLink: "https://github.com/5hishirH/BrandShop-client.git",
+      liveLink: "https://fashion-and-apparel-31c61.firebaseapp.com/",
+      img: "",
+    },
+  ];
+  return (
+    <div>
+      <div>
+        <h2>My Complete Projects</h2>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
